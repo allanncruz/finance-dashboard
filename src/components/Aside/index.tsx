fc229312365604f4dc01cd789811b95cc4d96d5b
allanncruz <contato@allancruz.com.br>
2021-02-20 13:53:33 -0300

    set folders and components

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  Container
+} from './styles'
+
+const Aside: React.FC = () => {
+  return(
+    <Container>
+      <h1>AS</h1>
+    </Container>
+  );
+}
+
+export default Aside;
