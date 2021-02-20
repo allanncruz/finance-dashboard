2e3fa76169d004382e7eb97ab2bf15179d73406c
allanncruz <contato@allancruz.com.br>
2021-02-20 10:52:54 -0300

    create finance dashboard app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Finance Dashboard
+      </header>
+    </div>
+  );
+}
+
+export default App;
