e43adfc9b4bc30b5e50f2dec958e81c9c9628bc0
allanncruz <contato@allancruz.com.br>
2021-02-20 15:09:19 -0300

    set colors for dark and light

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'dark',
+
+  colors:{
+    primary:'#1b1f38',
+    secondary:'#252a48',
+    terteari:'#303862',
+
+    white:'#fff',
+    black:'#000',
+    gray:'#bfbfbf',
+
+    success:'#5665f0',
+    info:'#f7932c',
+    warning:'#e44c4e',
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'light',
+
+  colors:{
+    primary:'#dcdcdc',
+    secondary:'#fff',
+    terteari:'#f5f5f5',
+
+    white:'#000',
+    black:'#fff',
+    gray:'#bfbfbf',
+
+    success:'#52bc86',
+    info:'#3a5998',
+    warning:'#f46761',
+  }
+}
